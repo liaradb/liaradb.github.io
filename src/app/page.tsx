c01b33ed8190cdbf337c5ce3982289f3089d0286
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-12-23 14:22:01 -0700

    Changed base path.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/liaradb/next.svg"
+          src="/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
