64ec6ba41e3fedce1b823dcc5ad88768cee3960a
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-12-23 14:16:40 -0700

    Created GitHub pages configuration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="/liaradb/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
