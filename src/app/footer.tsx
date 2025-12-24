f21c0242f21ed75fd893b75d751ab9d5b98e5dc0
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-12-24 09:54:05 -0700

    Created footer content.

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,17 +1,47 @@
-import { Box, Divider } from "@mui/material";
+import { Box, Button, Divider } from "@mui/material";
+import { FC } from "react";
 
 export const Footer = () => {
   return (
     <Box>
       <Divider />
-      <Box
-        padding={2}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        Footer
+      <Box padding={2} display="flex" alignItems="start">
+        <Box flexGrow={1}>
+          <Copyright />
+        </Box>
+        <Box>
+          <Links
+            links={[
+              { title: "About us", href: "/" },
+              { title: "License", href: "/" },
+            ]}
+          />
+        </Box>
       </Box>
     </Box>
   );
 };
+
+const year = new Date().getFullYear();
+const company = "Sean Johnson";
+const copyright = `${company} ${year}`;
+
+const Copyright = () => {
+  return <>&copy; {copyright}</>;
+};
+
+const Links: FC<{ links: { href: string; title: string }[] }> = ({ links }) => (
+  <Box component="ul" padding={0} margin={0}>
+    {links.map(({ href, title }) => (
+      <Link href={href} title={title} key={title} />
+    ))}
+  </Box>
+);
+
+const Link: FC<{ href: string; title: string }> = ({ href, title }) => (
+  <Box component="li" padding={0} sx={{ listStyleType: "none" }}>
+    <Button variant="text" LinkComponent="a" color="info" href={href}>
+      {title}
+    </Button>
+  </Box>
+);
--- src/app/header.tsx
+++ src/app/header.tsx
@@ -2,7 +2,6 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 
@@ -25,7 +24,6 @@ export function Header() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             {title}
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
