83372d2753d20dfe849af69da21b45c9077cbc6d
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-12-26 09:01:40 -0700

    Changed external links to open tabs.

--- src/app/header_icons.tsx
+++ src/app/header_icons.tsx
@@ -4,7 +4,11 @@ import { Box, IconButton } from "@mui/material";
 export const HeaderIcons = () => {
   return (
     <Box display="flex">
-      <IconButton LinkComponent="a" href="https://github.com/liaradb">
+      <IconButton
+        LinkComponent="a"
+        href="https://github.com/liaradb"
+        target="_blank"
+      >
         <GitHub />
       </IconButton>
     </Box>
