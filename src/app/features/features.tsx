f1d5a84506e75f786948b6a0f3ef59246174d61e
Sean Johnson <sean.johnson.arizona@gmail.com>
2025-12-28 11:22:28 -0700

    Created features content in MDX.

--- src/app/features/features.tsx
+++ src/app/features/features.tsx
@@ -1,55 +1,15 @@
+import { FC } from "react";
+
 import { AppPage } from "@/components";
-import { Box, Divider, Typography } from "@mui/material";
-import { FC, ReactNode } from "react";
+
+import FeaturesContent from "./features.mdx";
 
 export const Features: FC<{ title: string }> = ({ title }) => {
   return (
     <AppPage title={title} subTitle="Modern solutions to modern problems">
-      <Box>
-        <Typography variant="body1">
-          Architecture trends are different than they were 25 years ago. Domain
-          Driven Design has given way to Microservices and Event Streams, rather
-          than large monolithic SQL Databases. There is a simpler way, but our
-          infrastructure needs to support it.
-        </Typography>
-      </Box>
-
-      <Section title="Event-Log storage">
-        <Typography variant="body1"></Typography>
-      </Section>
-
-      <Section title="Key-Value storage">
-        <Typography variant="body1"></Typography>
-      </Section>
-
-      <Section title="Aggregate snapshots">
-        <Typography variant="body1"></Typography>
-      </Section>
-
-      <Section title="Transactional inserts">
-        <Typography variant="body1"></Typography>
-      </Section>
-
-      <Section title="Distributed database">
-        <Typography variant="body1"></Typography>
-      </Section>
-
-      <Section title="gRPC based communication">
-        <Typography variant="body1"></Typography>
-      </Section>
+      <div>
+        <FeaturesContent />
+      </div>
     </AppPage>
-  );
-};
-
-const Section: FC<{ title: string; children: ReactNode | ReactNode[] }> = ({
-  title,
-  children,
-}) => {
-  return (
-    <Box>
-      <Typography variant="h2">{title}</Typography>
-      <Divider />
-      {children}
-    </Box>
   );
 };
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,13 +1,40 @@
-import { Divider, Typography } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 import type { MDXComponents } from "mdx/types";
 import { LinkLink } from "./components";
 
 const components = {
   h1: ({ children }) => {
-    return <Typography variant="h1">{children}</Typography>;
+    return (
+      <Box marginTop={3} marginBottom={1}>
+        <Typography variant="h1">{children}</Typography>
+        <Divider />
+      </Box>
+    );
   },
   h2: ({ children }) => {
-    return <Typography variant="h2">{children}</Typography>;
+    return (
+      <Box marginTop={3} marginBottom={1}>
+        <Typography variant="h2">{children}</Typography>
+        <Divider />
+      </Box>
+    );
+  },
+  h3: ({ children }) => {
+    return (
+      <Box marginTop={3} marginBottom={1}>
+        <Typography variant="h3">{children}</Typography>
+        <Divider />
+      </Box>
+    );
+  },
+  h4: ({ children }) => {
+    return <Typography variant="h4">{children}</Typography>;
+  },
+  h5: ({ children }) => {
+    return <Typography variant="h5">{children}</Typography>;
+  },
+  h6: ({ children }) => {
+    return <Typography variant="h6">{children}</Typography>;
   },
   hr: ({ children }) => {
     return <Divider>{children}</Divider>;
